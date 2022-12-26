47e56b8b37e059c50121c354f05a9a1923623534
Sejo Basic <sejoob@gmail.com>
2022-12-26 13:28:40 -0700

    Display author info and image location

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(resp => resp.json())
-  .then(data => document.body.style.backgroundImage = `url(${data.urls.full})`)
+  .then(data => getUnsplashData(data))
+
+
+  function getUnsplashData(data) {
+    const authorName = `${data.user.first_name} ${data.user.last_name}`
+
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.querySelector(".author-name").textContent = authorName
+
+    if (data.location.name !== null) {
+      document.querySelector(".location-info").textContent = `${data.location.name}`
+    }
+  }
+
 
