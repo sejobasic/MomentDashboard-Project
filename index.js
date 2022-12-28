962f3116548ebf1ae53dd2bf36af2764e223927d
Sejo Basic <sejoob@gmail.com>
2022-12-28 16:04:47 -0700

    Fade background image in

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { getCurrentDateAndTime, getUserLocation, getRandomQuotes } from "./utils.js"
+import {
+  getCurrentDateAndTime,
+  getUserLocation,
+  getRandomQuotes,
+} from './utils.js'
 
 window.setInterval(function () {
   getCurrentDateAndTime()
@@ -10,14 +14,16 @@ getRandomQuotes()
 fetch(
   'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=landscape'
 )
-  .then(resp => resp.json())
-  .then(data => {
+  .then((resp) => resp.json())
+  .then((data) => {
     populateUnsplashData(data)
     console.log(data)
   })
   .catch((error) => {
     console.log(error)
-    document.body.style.backgroundImage = `url(https://images.pexels.com/photos/4245826/pexels-photo-4245826.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`
+    document.querySelector(
+      '.background-img'
+    ).style.backgroundImage = `url(https://images.pexels.com/photos/4245826/pexels-photo-4245826.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`
   })
 
 function populateUnsplashData(data) {
@@ -25,7 +31,9 @@ function populateUnsplashData(data) {
   const authorInfo = document.querySelector('.author-name')
   const authorName = `${data.user.first_name} ${data.user.last_name} / Unsplash`
 
-  document.body.style.backgroundImage = `url(${data.urls.full})`
+  document.querySelector(
+    '.background-img'
+  ).style.backgroundImage = `url(${data.urls.full})`
   authorInfo.textContent = authorName
   authorInfo.href = data.links.html
 
@@ -35,7 +43,3 @@ function populateUnsplashData(data) {
     ).textContent = `${data.location.name}`
   }
 }
-
-
-
-
